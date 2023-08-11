6dbcbed9a2d57a3b092efa9e4f6395c6087f7239
Aayush Rauniyar <arauniyar061@gmail.com>
2023-08-11 07:56:04 +0530

    reworking the worker_details files with using Gpt3

--- worker_details.py
+++ worker_details.py
@@ -10,18 +10,34 @@ def show_team_details():
     #st.sidebar.success("Select a demo above.")
 
     st.markdown(
-        """
-        This project has taught alots things to us like Team building the Team and balancing and distributing the workloads.
-        #### We builded and Contibuted as per our expertise to finish this projects.
-        ### Kishan Raj Rauniyar 
-        Kishan, the attacker & Frontend Developer of our team, is good with communciation skills not only he have very good skill designing the frontend part of our very first team project.
+        """        
+        ## Our Journey: Teamwork, Balance, and Expertise 
 
-        ### Kshitij Bohara
-        Kshitij, the defender & ML developer(Brain) of our team, is very good at developing ML models and not only that he is very versatile as developer and good competative programmer. 
-        - Check out LinkedIn [LinkedIn](https://www.linkedin.com/in/kshitij-bohara-ababb622a)
-        
-        ### Aayush Rauniyar
-        Aayush, the allrounder & cloud developer, R&D Expert of team, is good cloud developers and manages the all the development and distributing the work loads and also expertizes at doing research and analysis of data collection and also preparing prototype model.
-        - Check out LinkedIn [LinkedIn](https://www.linkedin.com/in/aayushrauniyar061/)
-    """
+Welcome to our world of collaboration, where we've mastered the art of teamwork, balance, and expertise. Our project has been a testament to these core values, shaping us into a cohesive and proficient unit.
+
+### Meet the Minds Behind the Magic
+
+**Kishan Raj Rauniyar - Crafting User Experiences**
+
+Kishan is not just a frontend developer; he's a communication maestro who bridges the gap between design and user interaction. With an artistic flair and impeccable communication skills, Kishan shapes captivating frontends that breathe life into our projects.
+
+**Kshitij Bohara - Sculpting Intelligence**
+
+Kshitij, our defender and machine learning virtuoso, wields algorithms like an artist's brush. His expertise in developing intricate ML models is complemented by his versatile coding skills and prowess in competitive programming. Connect with Kshitij on [LinkedIn](https://www.linkedin.com/in/kshitij-bohara-ababb622a) to witness his journey.
+
+**Aayush Rauniyar - Orchestrating the Cloud**
+
+An all-rounder extraordinaire, Aayush is our cloud development maven and the driving force behind research and development. From managing workloads to crafting prototype models, his cloud wizardry ensures seamless execution. Explore Aayush's profile on [LinkedIn](https://www.linkedin.com/in/aayushrauniyar061/) to delve into his expertise.
+
+## Our Project: AWS Salary Prediction
+
+### 🤔Curious about our latest endeavor? 
+Check out our project [AWS Salary Prediction](https://github.com/aayushRauniyar/aws-salary-prediction.git) on GitHub. It's a testament to our skills, dedication, and innovation, reflecting our commitment to excellence.
+
+## Join Our Voyage 🚀
+
+Our journey is a tapestry woven with skills, dedication, and innovation. As a team, we've learned, grown, and conquered challenges, creating a legacy that reflects our commitment to shaping futures.
+
+Come, be a part of our story. Together, we're not just building projects – we're crafting a future powered by possibilities.
+"""
     )
